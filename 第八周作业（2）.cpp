1aa5df8510b96ee0f9cb79c3c4ce31aa4becc9dd
bclnb <55117555+bclnb@users.noreply.github.com>
2019-10-26 22:03:54 +0800

    Add files via upload

---
+++ 第八周作业（2）.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main () 
+{
+	int x[5]= {1, 2, 3, 6, 9}, i, j, n;
+     scanf("%d %d %d %d %d", &x[0], &x[1], &x[2], &x[3], &x[4]);
+     n = 5;
+    for(i=1;i<=n-1;i++) 
+    {
+	  for(j=0;j<n-i;j++) 
+	 {
+       if( x[j] < x[j+1]) 
+	   {
+   	    int temp = x[j];
+   	    x[j] = x[j+1];
+   	    x[j+1] = temp;
+       }
+
+     }
+    }
+	printf(" %d%d%d%d%d", x[0], x[1], x[2], x[3], x[4]);
+   return 0;
+ 
+}
